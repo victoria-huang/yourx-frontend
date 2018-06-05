f479c4c5bbf1c1cf0789d434915468c046d58d58
victoria-huang <vctrah@gmail.com>
2018-06-05 16:44:30 -0400

    add auth gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  
+  namespace :api do
+    namespace :v1 do
+      resources :patients
+      resources :prescriptions
+      resources :doctors
+      resources :take_times
+      resources :prescription_take_times
+      resources :pharmacies
+    end
+  end
 end
