5f91e6ca607a5547f11ef8bb24bc2d44960d610d
victoria-huang <vctrah@gmail.com>
2018-06-06 09:20:44 -0400

    add react-redux and redux and initial store/root reducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { store } from './store.js'
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+// registerServiceWorker();
