54204651c9c98bc933f6065d2d33e010876b5832
victoria-huang <vctrah@gmail.com>
2018-06-06 17:07:27 -0400

    modified sessions_controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,17 +1,17 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
 
   def create_patient
-    @user = Patient.find_by(username: params[:username])
+    @user = Patient.find_by(username: params["username"])
     authenticate_user(@user)
   end
 
   def create_doctor
-    @user = Doctor.find_by(username: params[:username])
+    @user = Doctor.find_by(username: params["username"])
     authenticate_user(@user)
   end
 
   def authenticate_user(user)
-    if (user && user.authenticate(params[:password]))
+    if (user && user.authenticate(params["password"]))
       render json: token_json(user)
     else
       render json: {
@@ -19,5 +19,5 @@ class SessionsController < ApplicationController
       }, status: :unauthorized
     end
   end
-  
+
 end
