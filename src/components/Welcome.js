f2977b25158bb39b0e6155043de5df23d43f0ee9
victoria-huang <vctrah@gmail.com>
2018-06-06 16:58:20 -0400

    add styling to login pages

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -9,15 +9,15 @@ export default class Welcome extends Component {
   render() {
     return (
       <div className="welcome">
-        <div className="video-container">
+        <div className="content-container">
           <video autoPlay loop>
             <source src="https://dl.dropboxusercontent.com/s/fcwea9gqasw6gb1/medicine_packages.mp4" type="video/mp4" />
           </video>
         </div>
 
-        <div className="content">
+        <div className="content home">
           <img src={require("../assets/pill_logo.png")} height='80px' width='80px' />
-          <h1>Y o u R x</h1>
+          <h1 className="logo">Y o u R x</h1>
           <h2>Embrace your health.</h2>
           <br />
           <button name="patient" className="ui button" role="button" onClick={this.handleClick}>Patient Login</button>
--- src/components/patients/LoginForm.js
+++ src/components/patients/LoginForm.js
@@ -54,25 +54,38 @@ export default class LoginForm extends Component {
 
     return (
       <div>
-        <h1>{this.props.type} Login</h1>
+        <div className="content-container">
+          <img src={require('../../assets/blur_bg.jpg')} />
+        </div>
+        <div className="content auth">
+          { this.props.type === "Patient" ?
+            <i aria-hidden="true" className="pills huge icon"></i>
+          :
+            <i aria-hidden="true" className="user doctor huge icon"></i>
+          }
+          <h1 className="content-header">{this.props.type} Login</h1>
 
-        <ul>
-          { errors }
-        </ul>
+          <ul>
+            { errors }
+          </ul>
 
-        <form className="ui form" onSubmit={this.handleSubmit}>
-          <div className="field">
-            <label htmlFor="username">Username</label>
-            <input name="username" type="text" value={this.state.username} onChange={this.handleChange} />
-          </div>
+          <form className="ui form" onSubmit={this.handleSubmit}>
+            <div className="field">
+              <label htmlFor="username">Username</label>
+              <input name="username" type="text" value={this.state.username} onChange={this.handleChange} />
+            </div>
 
-          <div className="field">
-            <label htmlFor="password">Password</label>
-            <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
-          </div>
+            <div className="field">
+              <label htmlFor="password">Password</label>
+              <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+            </div>
 
-          <button type="submit" className="ui button" role="button">Login</button>
-        </form>
+            <button type="submit" className="ui button" role="button">Login</button>
+          </form>
+          <div className="ui horizontal divider"> OR </div>
+          <button className="ui button" role="button" onClick={() => this.props.history.push("/")}>Return to Welcome Page</button>
+
+        </div>
       </div>
     )
   }
