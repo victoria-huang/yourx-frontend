81dfda6a782a04f05b87dda36b249e7c5f168cd4
victoria-huang <vctrah@gmail.com>
2018-06-09 19:26:17 -0400

    add whenever gem to utilize cron job to reset taken boolean every 24 hours

--- app/models/prescription_take_time.rb
+++ app/models/prescription_take_time.rb
@@ -1,4 +1,9 @@
 class PrescriptionTakeTime < ApplicationRecord
   belongs_to :prescription
   belongs_to :take_time
+
+  def self.reset_taken
+    self.all.each { |t| t.update(taken: !t.taken) }
+    Rails.logger.info("TakeTimes updated at #{Time.now}")
+  end
 end
