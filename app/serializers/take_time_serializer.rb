2efb73721d35f0c1c55fc4ac39954957f6e11e7a
victoria-huang <vctrah@gmail.com>
2018-06-10 20:46:45 -0400

    editted formatting of rx_time

--- app/serializers/take_time_serializer.rb
+++ app/serializers/take_time_serializer.rb
@@ -2,6 +2,6 @@ class TakeTimeSerializer < ActiveModel::Serializer
   attributes :id, :day, :formatted_time
 
   def formatted_time
-    object.rx_time.strftime('%r')
+    object.rx_time.strftime("%l:%M %p")
   end
 end
