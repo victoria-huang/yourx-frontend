274f7e9fcc7570e4c6c01242e712f25a674aa341
victoria-huang <vctrah@gmail.com>
2018-06-11 10:31:55 -0400

    edited prescription serializer to include relationships

--- app/serializers/prescription_serializer.rb
+++ app/serializers/prescription_serializer.rb
@@ -1,3 +1,5 @@
 class PrescriptionSerializer < ActiveModel::Serializer
-  attributes :id, :brand_name, :generic_name, :rxcui, :image_url, :amount_per_dose, :dosage, :formulation, :route, :daily_freq, :weekly_freq, :start_date, :end_date
+  attributes :id, :brand_name, :generic_name, :rxcui, :image_url, :amount_per_dose, :dosage, :formulation, :route, :daily_freq, :weekly_freq, :start_date, :end_date, :prescription_take_times, :take_times
+  has_many :prescription_take_times
+  has_many :take_times, through: :prescription_take_times
 end
