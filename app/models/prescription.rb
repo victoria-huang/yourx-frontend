0821435acda7e080c5b0e96643dec8c1d125ea64
victoria-huang <vctrah@gmail.com>
2018-06-12 13:43:28 -0400

    remove dependent delete all for prescriptions & take times

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,7 +3,7 @@ class Prescription < ApplicationRecord
   belongs_to :pharmacy, optional: true
   belongs_to :patient
   has_many :prescription_take_times, dependent: :delete_all
-  has_many :take_times, through: :prescription_take_times, dependent: :delete_all
+  has_many :take_times, through: :prescription_take_times
 
   def daily_take_times
     self.take_times.select { |time| time.day === DateTime.now.strftime("%A") }
