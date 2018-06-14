3638f563968e282bf5dc77ce17ba5dabd0e7bfb4
victoria-huang <vctrah@gmail.com>
2018-06-14 13:39:17 -0400

    add styling and images to pillbox

--- src/components/patients/EveningMed.js
+++ src/components/patients/EveningMed.js
@@ -1,30 +1,47 @@
 import React from 'react'
 import EditPrescription from '../prescriptions/EditPrescription'
+import Popup from "reactjs-popup";
+
+function getRandomNum() {
+  return Math.floor((Math.random() * 4) + 1);
+}
 
 const EveningMed = (props) => {
   const times = props.times.map((t, idx) => {
     if (t.take_time.time_of_day === "evening") {
       return (
-        <div key={idx}>
-          <p>Take at {t.take_time.formatted_time}</p>
-          { props.times[idx].rx_take_time.taken && props.day === "today" ? "Taken" : null }
-          <EditPrescription
-            rxTakeTimeId={props.times[idx].rx_take_time.id}
-            timesIdx={idx}
-            prescriptionId={props.med.id}
-            day={props.day}
-            history={props.history}
-            text={props.times[idx].rx_take_time.taken ? 'Untake' : 'Take'}
-          />
-          <br />
-        </div>
+        <Popup key={idx} trigger={<img src={require(`../../assets/pill_${getRandomNum()}.png`)} height='40px' width='40px'/>} modal>
+          {close => (
+            <div className="modal">
+              <a className="close" onClick={close}>
+                &times;
+              </a>
+              <div className="header">{props.med.brand_name}</div>
+              <div className="content-modal">
+                {" "}
+                Take at {t.take_time.formatted_time}
+                <br />
+                { props.times[idx].rx_take_time.taken && props.day === "today" ? "Taken" : null }
+              </div>
+              <div className="actions">
+                <EditPrescription
+                  rxTakeTimeId={props.times[idx].rx_take_time.id}
+                  timesIdx={idx}
+                  prescriptionId={props.med.id}
+                  day={props.day}
+                  history={props.history}
+                  text={props.times[idx].rx_take_time.taken ? 'Untake' : 'Take'}
+                />
+              </div>
+            </div>
+          )}
+        </Popup>
       )
     }
   })
 
   return (
     <div>
-      <h4>{props.med.brand_name}</h4>
       { times }
     </div>
   )
--- src/components/patients/MedsContainer.js
+++ src/components/patients/MedsContainer.js
@@ -54,32 +54,36 @@ class MedsContainer extends Component {
         <div className="ui two column grid centered container">
           <div className="two column centered row">
             <div className="column">
-              <h2>Morning (6 AM - 12 PM)</h2>
+              <h2><img src={require('../../assets/morning.png')} height='50px' width='50px'/> Morning</h2>
+              <p>6 AM - 12 PM</p>
               <div className="ui inverted divider"></div>
-              <div className="ui four column grid centered container">
+              <div className="ui three column padded grid centered container">
                 { morningMeds.findIndex(e => e !== undefined) > -1 ? morningMeds : "None" }
               </div>
             </div>
             <div className="column">
-              <h2>Afternoon (12 PM - 6 PM)</h2>
+              <h2><img src={require('../../assets/afternoon.png')} height='50px' width='50px'/> Afternoon</h2>
+              <p>12 PM - 6 PM</p>
               <div className="ui inverted divider"></div>
-              <div className="ui four column grid centered container">
+              <div className="ui three column padded grid centered container">
                 { afternoonMeds.findIndex(e => e !== undefined) > -1 ? afternoonMeds : "None" }
               </div>
             </div>
           </div>
           <div className="two column centered row">
             <div className="column">
-              <h2>Evening (6 PM - 12 AM)</h2>
+              <h2><img src={require('../../assets/evening.png')} height='50px' width='50px'/> Evening</h2>
+              <p>6 PM - 12 AM</p>
               <div className="ui inverted divider"></div>
-              <div className="ui four column grid centered container">
+              <div className="ui three column padded grid centered container">
                 { eveningMeds.findIndex(e => e !== undefined) > -1 ? eveningMeds : "None" }
               </div>
             </div>
             <div className="column">
-              <h2>Night (12 AM - 6 AM)</h2>
+              <h2><img src={require('../../assets/night.png')} height='50px' width='50px'/> Night</h2>
+              <p>12 AM - 6 AM</p>
               <div className="ui inverted divider"></div>
-              <div className="ui four column grid centered container">
+              <div className="ui three column padded grid centered container">
                 { nightMeds.findIndex(e => e !== undefined) > -1 ? nightMeds : "None" }
               </div>
             </div>
