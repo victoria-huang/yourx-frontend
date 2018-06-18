a80a34ea5c4934c1effb6a69458bc24689f9b837
victoria-huang <vctrah@gmail.com>
2018-06-18 14:27:38 -0400

    add dose and image_url to params/attributes for prescriptions

--- app/serializers/med_serializer.rb
+++ app/serializers/med_serializer.rb
@@ -1,4 +1,4 @@
 class MedSerializer < ActiveModel::Serializer
-  attributes :id, :brand_name, :rxcui, :sig
-  # :generic_name, :rxcui, :image_url, :amount_per_dose, :dosage, :formulation, :route, :daily_freq, :weekly_freq, :start_date, :end_date
+  attributes :id, :brand_name, :rxcui, :sig, :dosage, :image_url
+  # :generic_name, :amount_per_dose, :formulation, :route, :daily_freq, :weekly_freq, :start_date, :end_date
 end
